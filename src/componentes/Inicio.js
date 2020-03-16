d0002c20485b2b61126072f83cc22055dc106516
CBNaS <protocolcbr@gmail.com>
2020-03-16 14:59:33 -0600

    Cambios minimos

---
+++ src/componentes/Inicio.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+
+export default class Inicio extends Component {
+    render() {
+        return (
+            <div>
+                Pagina principal
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+
+                <p>
+                    Parrafo
+                </p>
+            </div>
+        )
+    }
+}
